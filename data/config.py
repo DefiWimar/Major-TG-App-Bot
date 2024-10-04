cccc7c19eedb269e16085fa62757bdb8a9025d42
DefiWimar <wimarxt@gmail.com>
2024-10-04 15:30:47 +0200

    Add files via upload

---
+++ data/config.py
@@ -0,0 +1,54 @@
+# api id, hash
+API_ID = 11111111
+API_HASH = 'Your api hash'
+
+USE_TG_BOT = False # True if you want to use Telegram bot, else False
+BOT_TOKEN = '' # API TOKEN from @BotFather
+CHAT_ID = '' # Your Telegram ID
+
+# delay between connecting to accounts
+ACC_DELAY = [5, 150]
+
+# type of proxy
+PROXY_TYPE = "socks5" # http/socks5
+
+# folder with sessions (do not change)
+WORKDIR = "sessions/"
+
+# use proxy
+USE_PROXY = True # True/False
+
+# referral code, follows startapp=
+REF_CODE = '707649803'
+
+# delay between tasks
+TASK_SLEEP = [30, 60] #[min, max]
+
+# delay between minigames
+GAME_SLEEP = [30, 120] #[min, max]
+
+# task IDs to be checked
+BLACKLIST_TASKS = {33, 21, 27, 20, 36, 80, 77}
+
+# play this minigame True/False
+HOLD_COIN = True
+ROULETTE = True
+SWIPE_COIN = True
+
+# mini delays
+MINI_SLEEP = [5, 15] #[min, max]
+
+# delay between cycles
+BIG_SLEEP = [5*60*60, 8*60*60]
+
+# join squad
+JOIN_SQUAD = True
+
+hello ='''   
+ ____        __ ___        ___                      
+|  _ \  ___ / _(_) \      / (_)_ __ ___   __ _ _ __ 
+| | | |/ _ \ |_| |\ \ /\ / /| | '_ ` _ \ / _` | '__|
+| |_| |  __/  _| | \ V  V / | | | | | | | (_| | |   
+|____/ \___|_| |_|  \_/\_/  |_|_| |_| |_|\__,_|_|             
+
+'''
